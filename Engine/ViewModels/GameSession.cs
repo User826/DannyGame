660b7d1f783a0be0ea2cdedf4a01abff0a7d6842
Me <Me@DESKTOP-0SGLJOQ>
2023-05-22 01:17:34 -0400

    Add project files.

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Danny";
+            CurrentPlayer.CharacterClass = "Wizard";
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 10000;
+        }
+    }
+}
