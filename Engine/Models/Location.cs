ebbbec0d499e88fc125b960702aa4430d0837998
User826 <dannywu826@gmail.com>
2023-05-23 01:09:34 -0400

    Added a Location Class

---
+++ Engine/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Location
+    {
+        public int Location_Row { get; set; }   
+        public int Location_Column { get; set;}
+        public string Name { get; set;}
+        public string Description { get; set;}
+        public string ImageName { get; set;}
+
+    }
+}
